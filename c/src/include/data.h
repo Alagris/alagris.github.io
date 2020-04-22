e542e5fcf15ede72c239a3ed0d3bdcad8f47826b
Alagris <alagris12358@gmail.com>
2020-04-22 13:56:38 +0200

    restrucutre project; add placeholder for emscripten

---
+++ c/src/include/data.h
@@ -0,0 +1,45 @@
+
+#ifndef DATA_H
+#define DATA_H
+/*This is used for alphabet and indexing states. Let R be a set of integers between r_min and r_max inclusive*/
+
+#define r long long unsigned int
+#define r_max 18446744073709551615
+#define r_min 0
+#define r_epsilon r_max
+#define r_no_state r_max
+/*This is used for booleans*/
+#define b unsigned char
+
+/*Generic pair that could for instance hold indices of two vertices, or vertex and alphabet char*/
+
+struct Transition{
+	r destinationState;
+	r inputLabelRangeBeginInclusive;
+	r inputLabelRangeEndExclusive;
+	r* output;
+};
+
+struct Mealy{
+	r stateCount;
+	b * acceptingStates;
+	struct Transition ** delta;
+	
+};
+struct FunctionalTransition{
+	r destinationState;
+	r inputLabelRangeBeginInclusive;
+	r inputLabelRangeEndExclusive;
+	r * cellTransitions;
+	r * cellTransitionLabels;
+};
+
+struct FunctionalMealy{
+	r stateCount;
+	r cellsCount;
+	r * acceptingCellPerState;
+	
+	struct FunctionalTransition ** delta;
+	
+};
+#endif
