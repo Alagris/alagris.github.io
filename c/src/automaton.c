b1b18dd1efa1084924b3853b2ce59b3ae351c56c
Marcin Jabłoński <marcin@marcinjab.pl>
2020-06-12 06:00:54 +0200

    minor data struct fixes

--- c/src/automaton.c
+++ c/src/automaton.c
@@ -1,27 +1,28 @@
-#include "automaton.h"
+#include "data.h"
 
-Mealy * initializeMealy() {
-    Mealy * mealy = (Mealy *) malloc(sizeof(Mealy));
-    State * states = (State *) malloc(BASIC_STATE_ARRAY_SIZE * sizeof(State));
-    Transition * transitions = (Transition *) malloc(BASIC_TRANSITION_ARRAY_SIZE * sizeof(Transition));
-    states->transitions = transitions;
-    mealy->states[0] = states;
-    mealy->stateCount = 0;
-    return mealy;
+Mealy * initializeMealy(r stateCount, r acceptingStateCount) {
+    // r acceptingStateBufferSize = (acceptingStateCount + sizeof(b) - 1) / sizeof(b);
+    // Mealy * mealy = (Mealy *) malloc(sizeof(Mealy));
+    // State * states = (State *) malloc(stateCount);
+    // Transition * transitions = (Transition *) malloc(BASIC_TRANSITION_ARRAY_SIZE * sizeof(Transition));
+    // states->transitions = transitions;
+    // mealy->states[0] = states;
+    // mealy->stateCount = 0;
+    // return mealy;
 }
 
 int addState(Mealy * mealy) { 
-    State * newState = calcOffsetAndExtend(mealy->stateCount);
-    if(newState == 0)
-        return -1;
-    mealy->stateCount += 1;
-    return 0;
+    // State * newState = calcOffsetAndExtend(mealy->stateCount);
+    // if(newState == 0)
+    //     return -1;
+    // mealy->stateCount += 1;
+    // return 0;
 }
 
 int addTransition(Mealy * mealy, r state, t Transition,
-                  r destinationState, r inputLabelRangeBeginInclusive,
-                  r inputLabelRangeEndExclusive, t weight, char * output) {
-   State * state = calcStateOffset(state);
-   state[transition] = {destinationState, inputLabelRangeBeginInclusive,
-                        inputLabelRangeEndExclusive, weight, output};
+//                   r destinationState, r inputLabelRangeBeginInclusive,
+//                   r inputLabelRangeEndExclusive, t weight, char * output) {
+//    State * state = calcStateOffset(state);
+//    state[transition] = {destinationState, inputLabelRangeBeginInclusive,
+//                         inputLabelRangeEndExclusive, weight, output};
 }
--- c/src/include/data.h
+++ c/src/include/data.h
@@ -1,7 +1,8 @@
-
 #ifndef DATA_H
 #define DATA_H
 /*This is used for alphabet and indexing states. Let R be a set of integers between r_min and r_max inclusive*/
+
+#include "stdint.h"
 
 #define r long long unsigned int
 #define r_max 18446744073709551615
@@ -10,36 +11,47 @@
 #define r_no_state r_max
 /*This is used for booleans*/
 #define b unsigned char
+#
+
+typedef int16_t c;
 
 /*Generic pair that could for instance hold indices of two vertices, or vertex and alphabet char*/
 
-struct Transition{
+typedef struct Transition {
 	r destinationState;
-	r inputLabelRangeBeginInclusive;
-	r inputLabelRangeEndExclusive;
-	r* output;
-};
+	c inputLabelRangeBeginInclusive;
+	c inputLabelRangeEndExclusive;
+	char * output;
+} Transition;
 
-struct Mealy{
+typedef struct State
+{
+	Transition ** transitions;
+} State;
+
+typedef struct Mealy{
 	r stateCount;
 	b * acceptingStates;
-	struct Transition ** delta;
-	
-};
-struct FunctionalTransition{
-	r destinationState;
-	r inputLabelRangeBeginInclusive;
-	r inputLabelRangeEndExclusive;
-	r * cellTransitions;
-	r * cellTransitionLabels;
-};
+	r * entryState;
+	State * states;
+} Mealy;
 
-struct FunctionalMealy{
-	r stateCount;
-	r cellsCount;
-	r * acceptingCellPerState;
+
+// typedef struct FunctionalTransition{
+// 	r destinationState;
+// 	r inputLabelRangeBeginInclusive;
+// 	r inputLabelRangeEndExclusive;
+// 	r * cellTransitions;
+// 	r * cellTransitionLabels;
+// } FunctionalTransition;
+
+// typedef struct FunctionalMealy{
+// 	r stateCount;
+// 	r cellsCount;
+// 	r * acceptingCellPerState;
 	
-	struct FunctionalTransition ** delta;
+// 	struct FunctionalTransition ** delta;
 	
-};
+// } FunctionalMealy;
+
 #endif
