b4b10d6c33ed34780caf605827ac5e0bdf120227
Bohdan Bondar <bogbond@gmail.com>
2021-01-07 01:03:23 +0100

    imorove tests/ improve design

--- IntergationTests/Scrape.py
+++ IntergationTests/Scrape.py
@@ -2,16 +2,32 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 
+count = 0
+
+def check(value):
+    global count
+
+    if value == True:
+        print("Step "+ str(count) + " pass")
+    else:
+        print("Step "+ str(count) + " not pass")
+    count =count +1
+        
+
+
 options = Options()
 options.headless = True
 driver = webdriver.Firefox(options=options)
-driver.get("http://www.python.org")
+
+driver.get("http://localhost/compiler1")
 title = driver.title
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
+check("Solomonoff" in driver.title)
+
+elem = driver.find_element_by_name("inp")
 elem.clear()
-elem.send_keys("pycon")
+elem.send_keys(":eval x \'hellllllo\'")
 elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+check("Transducer \'y\'" not in driver.page_source)
+
 driver.close()
 
