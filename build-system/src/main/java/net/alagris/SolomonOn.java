ad474771a6a6580d9bf5f33c79102ef6cd2e3083
Marcin Jabłoński <marcin@marcinjab.pl>
2021-01-09 17:25:22 +0100

    Refactor cli part

--- build-system/src/main/java/net/alagris/SolomonOn.java
+++ build-system/src/main/java/net/alagris/SolomonOn.java
@@ -1,7 +1,9 @@
 package net.alagris;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 
 import picocli.CommandLine;
 import picocli.CommandLine.*;
@@ -12,8 +14,11 @@ import static net.alagris.OptimisedLexTransducer.makeEmptyExternalPipelineFuncti
         description = "Solomonoff compiler")
 class Compiler implements Callable<Integer> {
 
-    @Parameters(index = "0", defaultValue = "build", description = "\nbuild\nrun\ninteractive")
-    private String command;
+    @Parameters(index = "0", defaultValue = "build", description = "${COMPLETION-CANDIDATES}")
+    private Mode mode;
+
+    @Parameters(index = "1", description = "File to evaluate")
+    private File file;
 
     @Option(names = {"-f", "--file"}, description = "file to run")
     private String inputFile;
@@ -24,30 +29,26 @@ class Compiler implements Callable<Integer> {
     @Option(names = {"-n", "--no-minimization"}, description = "disable minimization")
     private boolean disableMinimimization;
 
+    @Option(names = {"-i", "--interactive"},
+            description = "Enable interactive mode. Useful with build or with run")
+    private boolean interactive;
+
+    private enum Mode { run, build, interactive }
+
+
     @Override
     public Integer call() throws Exception { // your business logic goes here...
-        OptimisedLexTransducer.OptimisedHashLexTransducer compiler = null;
 
-        if (command.equals("interactive")) {
-            compiler = new OptimisedLexTransducer.OptimisedHashLexTransducer(
-                    (!disableMinimimization),
+        if (Mode.build.compareTo(mode) == 0) {
+            OptimisedLexTransducer.OptimisedHashLexTransducer compiler =
+                    new OptimisedLexTransducer.OptimisedHashLexTransducer(
                     0,
-                    Integer.MAX_VALUE,
-                    makeEmptyExternalPipelineFunction());
-        } else if (command.equals("run") || command.equals("build")) {
-            compiler = new OptimisedLexTransducer.OptimisedHashLexTransducer(
-                    0,
-                    Integer.MAX_VALUE,
-                    false);
+                    Integer.MAX_VALUE);
+
             SolomonoffBuildSystem.run(buildFile, compiler.specs, i -> {
                 compiler.specs.pseudoMinimize(i);
                 return i;
             });
-
-        } else {
-            System.err.println("Invalid command. Try -h for help");
-            return 1;
-        }
 
         final ReplInfrastruct.Repl repl = new ReplInfrastruct.Repl(compiler);
 
