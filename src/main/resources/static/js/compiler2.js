50dabd45a5b5d835e85a41730a81b0e1e660f51d
Bohdan Bondar <bogbond@gmail.com>
2021-01-12 19:21:44 +0100

    small fixes and improve functionality

---
+++ src/main/resources/static/js/compiler2.js
@@ -0,0 +1,213 @@
+function saveStaticDataToFile() {
+    var val2 = outputField.value;
+    var val1 = editor.getSession().getValue();
+    var val4 = "Code: \n\n";
+    var val5 = "\n\nConsole:  \n\n";
+    var val3 = val4 + val1 + val5 + val2;
+    var blob = new Blob([val3], {
+        type: "text/plain;charset=utf-8"
+    });
+    saveAs(blob, "SavedGrammar.mealy");
+}
+
+
+replHistory = []
+replHistoryIndex = 0
+replCurrent = ""
+const MAX_REPL_LENGTH = 3000
+async function listAutomata() {
+    const response = await fetch('list_automata', {
+        method: 'POST'
+    })
+    const replResult = await response.text()
+    // const automata = JSON.parse(replResult)
+    // var ll = "";
+    // while (automataHtmlList.firstChild)
+    //     automataHtmlList.removeChild(automataHtmlList.firstChild);
+    // for(var i=0;i<automata.length;i++){
+    //    var item = document.createElement('li');
+    //     ll = ll+automata[i]+" ,";
+    // Set its contents:
+    //     item.appendChild(document.createTextNode(automata[i]));
+
+    // Add it to the list:
+    //     automataHtmlList.appendChild(item);
+    //  }
+    //var tok = automataHtmlList + "";
+    // automataHtmlList.toString();
+    // automataHtmlList.join('; ');
+    //var fok = "Define variables: " + ll;
+    tips.innerText = replResult;
+
+
+    //  document.getElementsByName('inp')[0].placeholder=fok;
+
+}
+
+function escapeCookie(cookieText) {
+    escaped = ''
+    for (var i = 0; i < cookieText.length; i++) {
+        if (cookieText.charAt(i) == ';') {
+            escaped = escaped + '%s'
+        } else if (cookieText.charAt(i) == '%') {
+            escaped = escaped + '%p'
+        } else if (cookieText.charAt(i) == '\n') {
+            escaped = escaped + '%n'
+        } else {
+            escaped = escaped + cookieText.charAt(i)
+        }
+    }
+    return escaped
+}
+
+function unescapeCookie(cookieText) {
+    unescaped = ''
+    for (var i = 0; i < cookieText.length; i++) {
+        if (cookieText.charAt(i) == '%') {
+            if (cookieText.charAt(i + 1) == 's') {
+                unescaped = unescaped + ';'
+            } else if (cookieText.charAt(i + 1) == 'p') {
+                unescaped = unescaped + '%'
+            } else if (cookieText.charAt(i + 1) == 'n') {
+                unescaped = unescaped + '\n'
+            }
+            i++
+        } else {
+            unescaped = unescaped + cookieText.charAt(i)
+        }
+    }
+    return unescaped
+}
+
+function setCookie(key, value) {
+    document.cookie = key + "=" + escapeCookie(value) + ";max-age=" + (60 * 60 * 60) + ";"
+}
+
+function getCookie(key) {
+    matches = document.cookie.match(';\\s*' + key + '=([^;]*)')
+    if (matches == null) return null;
+    return unescapeCookie(matches[1])
+}
+
+
+
+
+async function repl(replCommand) {
+    replCommand = replCommand.trim()
+    if (replCommand === "") {
+        replCommand = " "
+    }
+    const input = replCommand.match(/\S+/g)
+    const firstWord = input == null || input.length === 0 ? null : input[0]
+    if (firstWord == ":clear") {
+        outputField.value = ""
+    } else if (firstWord == ":load") {
+        const code = editor.getValue().trim()
+        setCookie('code', code)
+        const response = await fetch('upload_code', {
+            method: 'POST',
+            body: code
+        })
+        const errorMessage = await response.text()
+    }
+    const response = await fetch('repl', {
+        method: 'POST',
+        body: replCommand
+    })
+    const replResult = JSON.parse(await response.text())
+    if (firstWord == ":vis" && !replResult.wasError) {
+        console.log(replResult.output)
+        const workerURL = 'js/full.render.js';
+        let viz = new Viz({
+            workerURL
+        });
+        viz.renderSVGElement(replResult.output).then(function(element) {
+            ji.innerHTML = '';
+            ji.appendChild(element);
+            var modal = document.getElementById("myModal");
+            modal.style.display = "block";
+        }).catch(error => {
+            // Create a new Viz instance (@see Caveats page for more info)
+            viz = new Viz({
+                workerURL
+            });
+
+            // Possibly display the error
+            console.error(error);
+        });
+    }
+
+
+    if (replCommand != null) {
+        replHistory.push(replCommand)
+        replHistoryIndex = replHistory.length
+        toBeAppended = '> ' + replCommand + '\n' + (replResult.output).trim()
+    } else {
+        toBeAppended = (replResult.output).trim()
+    }
+    if (outputField.value.endsWith('\n')) {
+        outputField.value += toBeAppended
+    } else {
+        outputField.value += '\n' + toBeAppended
+    }
+    outputField.scrollTop = outputField.scrollHeight
+    replCurrent = inputField.value = ""
+
+    if (replResult.wasError) {
+        outputField.style.borderWidth = 'thick'
+        outputField.style.borderColor = "red"
+        rangeID = null
+        console.log(replResult)
+        if (replResult.col > -1) {
+            range = new Range(replResult.row, replResult.col - 1, replResult.row, replResult.col + 1)
+            rangeID = editor.session.addMarker(range, "red", "text");
+            console.log("rangeID=" + rangeID)
+        }
+
+        setTimeout(function() {
+            outputField.style.borderWidth = 'thin'
+            outputField.style.borderColor = "black"
+        }, 1500);
+        if (rangeID != null) {
+            editor.session.removeMarker(rangeID, "red", "text");
+        }
+    }
+    listAutomata()
+
+}
+
+function htmlDecode(input) {
+    var doc = new DOMParser().parseFromString(input, "text/html");
+    return doc.documentElement.textContent;
+}
+window.onload = function() {
+    codeSavedInCookie = getCookie('code')
+    if (codeSavedInCookie == null) return
+    editor.setValue(codeSavedInCookie);
+    outputField.scrollTop = outputField.scrollHeight
+}
+
+function compile() {
+    if (isSelected) {
+        repl(editor.getSelectedText())
+    } else {
+        repl(':load')
+    }
+}
+
+function closeModel() {
+    var Cmodal = document.getElementById("myModal");
+    Cmodal.style.display = "none";
+}
+
+
+function insert(text) {
+    var customPosition = {
+        row: 0,
+        column: 0
+    };
+    //var text = "x = 'trt'";
+    editor.setValue(text);
+    //editor.session.insert(customPosition , text);
+}
+
---
+++ src/main/resources/static/js/compiler2_after_html.js
@@ -0,0 +1,329 @@
+var langTools = ace.require("ace/ext/language_tools");
+var Range = ace.require("ace/range").Range;
+var editor = ace.edit("editor");
+editor.session.setMode("ace/mode/mealy");
+editor.setTheme("ace/theme/monokai");
+editor.setBehavioursEnabled(true)
+
+editorChangeCounter = 0
+editor.getSession().on('change', function() {
+    editorChangeCounter = (editorChangeCounter + 1) % 5
+    if (editorChangeCounter === 4) {
+        setCookie('code', editor.getSession().getValue())
+    }
+});
+
+editor.setOptions({
+    enableBasicAutocompletion: [{
+        getCompletions: (editor, session, pos, prefix, callback) => {
+            // note, won't fire if caret is at a word that does not have these letters
+            callback(null, [{
+                    name: 'subtract[',
+                    value: 'subtract[]',
+                    score: 1,
+                    meta: 'difference of two languages'
+                },
+                {
+                    name: 'rpni!(',
+                    value: 'rpni!()',
+                    score: 1,
+                    meta: 'RPNI inference algorithm'
+                },
+                {
+                    name: 'rpni_mealy!(',
+                    value: 'rpni_mealy!()',
+                    score: 1,
+                    meta: 'RPNI for Mealy machiens'
+                },
+                {
+                    name: 'ostia!(',
+                    value: 'ostia!()',
+                    score: 1,
+                    meta: 'OSTIA inference for transducers'
+                },
+                {
+                    name: 'compose[',
+                    value: 'compose[]',
+                    score: 1,
+                    meta: 'transducer composition'
+                },
+                {
+                    name: 'inverse[',
+                    value: 'inverse[]',
+                    score: 1,
+                    meta: 'transducer inversion'
+                },
+                {
+                    name: '/**/',
+                    value: '/**/',
+                    score: 1,
+                    meta: 'block comment'
+                },
+                {
+                    name: '//',
+                    value: '//',
+                    score: 1,
+                    meta: 'line comment'
+                },
+            ]);
+        },
+    }],
+    enableSnippets: true,
+    enableLiveAutocompletion: true
+});
+
+isSelected = false
+editor.selection.on('changeSelection', function() {
+    if (editor.getSelectedText() === "") {
+        if (isSelected) {
+            btnn.innerText = "Compile"
+            isSelected = false
+        }
+    } else {
+        if (!isSelected) {
+            btnn.innerText = "Compile selection"
+            isSelected = true
+        }
+    }
+});
+
+
+
+var inputField = ace.edit("inputField");
+inputField.setOptions({
+    maxLines: 1, // make it 1 line
+    autoScrollEditorIntoView: true,
+    highlightActiveLine: false,
+    printMargin: false,
+    showGutter: false,
+    mode: "ace/mode/mealy",
+    theme: "ace/theme/monokai"
+});
+
+inputField.setBehavioursEnabled(true)
+
+inputField.on("paste", function(e) {
+    e.text = e.text.replace(/[\r\n]+/g, " ");
+});
+inputField.commands.bindKey("Enter|Shift-Enter", function(e) {
+    repl(inputField.getValue())
+    inputField.setValue('')
+});
+inputField.commands.bindKey("Down", function(e) {
+    if (replHistoryIndex + 1 < replHistory.length) {
+        replHistoryIndex++
+        inputField.setValue(replHistory[replHistoryIndex])
+        inputField.selection.clearSelection()
+    } else if (replHistoryIndex + 1 === replHistory.length) {
+        replHistoryIndex++
+        inputField.setValue(replCurrent)
+        inputField.selection.clearSelection()
+    }
+});
+inputField.commands.bindKey("Up", function(e) {
+    if (replHistoryIndex > 0) {
+        if (replHistoryIndex === replHistory.length) {
+            replCurrent = inputField.value
+        }
+        replHistoryIndex--
+        inputField.setValue(replHistory[replHistoryIndex])
+        inputField.selection.clearSelection()
+    }
+});
+
+function update(event) {
+    var shouldShow = !inputField.session.getValue().length;
+    var node = inputField.renderer.emptyMessageNode;
+    if (!shouldShow && node) {
+        inputField.renderer.scroller.removeChild(inputField.renderer.emptyMessageNode);
+        inputField.renderer.emptyMessageNode = null;
+    } else if (shouldShow && !node) {
+        node = inputField.renderer.emptyMessageNode = document.createElement("div");
+        node.textContent = "Console input (enter to submit):"
+        node.className = "ace_emptyMessage"
+        node.style.padding = "0 9px"
+        node.style.position = "absolute"
+        node.style.zIndex = 9
+        node.style.opacity = 0.5
+        inputField.renderer.scroller.appendChild(node);
+    }
+}
+
+inputField.getSession().on('change', update);
+setTimeout(update, 100);
+inputField.setOptions({
+    enableBasicAutocompletion: [{
+        getCompletions: (editor, session, pos, prefix, callback) => {
+            // note, won't fire if caret is at a word that does not have these letters
+            callback(null, [{
+                    name: 'subtract[',
+                    value: 'subtract[]',
+                    score: 1,
+                    meta: 'difference of two languages'
+                },
+                {
+                    name: 'rpni!(',
+                    value: 'rpni!()',
+                    score: 1,
+                    meta: 'RPNI inference algorithm'
+                },
+                {
+                    name: 'rpni_mealy!(',
+                    value: 'rpni_mealy!()',
+                    score: 1,
+                    meta: 'RPNI for Mealy machiens'
+                },
+                {
+                    name: 'ostia!(',
+                    value: 'ostia!()',
+                    score: 1,
+                    meta: 'OSTIA inference for transducers'
+                },
+                {
+                    name: 'compose[',
+                    value: 'compose[]',
+                    score: 1,
+                    meta: 'transducer composition'
+                },
+                {
+                    name: 'inverse[',
+                    value: 'inverse[]',
+                    score: 1,
+                    meta: 'transducer inversion'
+                },
+                {
+                    name: ':eval',
+                    value: ':eval',
+                    snippet: ':eval ${1:transducer_name} \'${2:input_string}\'',
+                    score: 1,
+                    meta: 'evaluate transducer'
+                },
+                {
+                    name: ':ls',
+                    value: ':ls',
+                    score: 1,
+                    meta: 'list defined transducers'
+                },
+                {
+                    name: ':unset',
+                    value: ':unset',
+                    snippet: ':unset ${1:transducer_name}',
+                    score: 1,
+                    meta: 'undefine transducer'
+                },
+                {
+                    name: ':reset',
+                    value: ':reset',
+                    score: 1,
+                    meta: 'undefine all transducers'
+                },
+                {
+                    name: ':vis',
+                    value: ':vis',
+                    snippet: ':vis ${1:transducer_name}',
+                    score: 1,
+                    meta: 'visualizes automaton'
+                },
+                {
+                    name: ':pipes',
+                    value: ':pipes',
+                    score: 1,
+                    meta: 'list pipelines'
+                },
+                {
+                    name: ':size',
+                    value: ':size',
+                    snippet: ':size ${1:transducer_name}',
+                    score: 1,
+                    meta: 'print size of automaton'
+                },
+                {
+                    name: ':load',
+                    value: ':load',
+                    score: 1,
+                    meta: 'reload source file'
+                },
+                {
+                    name: ':is_det',
+                    value: ':is_det',
+                    snippet: ':is_det ${1:transducer_name}',
+                    score: 1,
+                    meta: 'check determinism'
+                },
+                {
+                    name: ':equal x y',
+                    value: ':equal',
+                    snippet: ':equal ${1:transducer_name1} ${2:transducer_name2}',
+                    score: 1,
+                    meta: 'check equivalence'
+                },
+                {
+                    name: ':run',
+                    value: ':run',
+                    snippet: ':run @${1:transducer_name} \'${2:input_string}\'',
+                    score: 1,
+                    meta: 'evaluate pipeline'
+                },
+                {
+                    name: ':clear',
+                    value: ':clear',
+                    score: 1,
+                    meta: 'clears console'
+                },
+                {
+                    name: ':rand_sample of_size',
+                    value: ':rand_sample',
+                    snippet: ':rand_sample ${1:transducer_name} of_size ${2:number}',
+                    score: 1,
+                    meta: 'randomly generate sample'
+                },
+                {
+                    name: ':rand_sample of_length',
+                    value: ':rand_sample',
+                    snippet: ':rand_sample ${1:transducer_name} of_length ${2:number}',
+                    score: 1,
+                    meta: 'randomly generate sample'
+                },
+            ]);
+        },
+    }],
+    enableSnippets: true,
+    enableLiveAutocompletion: true
+});
+
+
+
+if (document.cookie.match('(?<=(;|^)\\s*FirstTime=)[^;]*') == null) {
+    document.cookie = "FirstTime=; Max-Age=216000"
+
+
+    var enjoyhint_instance = new EnjoyHint({});
+    var enjoyhint_script_steps = [{
+            'next #editor': 'You can write Solomonoff code in this editor'
+        },
+        {
+            'next #inputField': 'Write REPL input in this line. Hit enter to submit. Use up and down arrow keys to traverse history.'
+        },
+        {
+            'next #btnn': 'Then click this button to compile your code'
+        },
+        {
+            'next #outputField': 'After compiling, you can interact with the automata from REPL'
+        },
+
+        {
+            'next #btnn1': 'If you want to save your changes for later or work with them locally, you can conveniently click this button'
+        },
+        {
+            'next #tutorial': 'Go ahead and check out the interactive tutorial we made speically for you'
+        },
+        {
+            'next #codeTutorial': 'If you click on any sample, it will automatically appear in the correct places'
+        },
+
+
+
+    ];
+    enjoyhint_instance.set(enjoyhint_script_steps);
+    enjoyhint_instance.run();
+}
