a34a28233a5b1669e0d0906ad43bc981bbace10d
Bohdan Bondar <bogbond@gmail.com>
2021-01-14 17:13:18 +0100

    fix repl after pressing back button

--- src/main/resources/static/js/compiler2.js
+++ src/main/resources/static/js/compiler2.js
@@ -240,3 +240,12 @@ function insert(text) {
     //editor.session.insert(customPosition , text);
 }
 
+window.addEventListener( "pageshow", function ( event ) {
+  var historyTraversal = event.persisted ||
+                         ( typeof window.performance != "undefined" &&
+                              window.performance.navigation.type === 2 );
+  if ( historyTraversal ) {
+    // Handle page restore.
+    window.location.reload();
+  }
+});
